866cbb6bed75ba695bd1cd13e00e416f7346a440
Dylan Vu <dylanvu9@gmail.com>
2022-12-22 01:44:41 -0800

    fix homepage styling for mobile

--- components/home/RecipeBlock.tsx
+++ components/home/RecipeBlock.tsx
@@ -1,14 +1,12 @@
 
 import styles from "../../styles/home/RecipeBlock.module.css";
 import RecipeCard from "../common/RecipeCard";
-import { RecipeCard as RecipeCardInterface } from "../../interfaces/components/recipe";
 import { useState } from "react";
 import { useRouter } from "next/router";
 import { Progress } from "@chakra-ui/react";
 import { Recipe } from "../../interfaces/data/recipe";
 
 const RecipeBlock = ({ imgLeft, card, tilt, text }: { imgLeft?: boolean, card: Recipe, tilt?: "left" | "right", text: "rating" | "background" | "postCooking" }) => {
-    const sample = `It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.\n\nThe point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).`
     const router = useRouter();
 
     const [clicked, toggleClicked] = useState<boolean>(false);
